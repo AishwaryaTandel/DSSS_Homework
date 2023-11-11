1f86439762b25f8f76b019207562cd473d50c155
AishwaryaTandel <aishwaryatandel1@gmail.com>
2023-11-11 23:19:09 +0100

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup File necessary for Python Packaging"""
+from setuptools import find_packages, setup
+
+setup(
+    name="math_quiz",
+    version="1.0",
+    author="Aishwarya Tandel(bu54roca)",
+    author_email="aishwarya.tandel@fau.de",
+    packages=find_packages(),
+)
